e630867d03fc6b29545efa023189264a95892c96
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-11-30 15:48:27 -0500

    commit tree db928d8001bb31b81a90e9b3d704ec4163e9c223

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(obj, key, value){
+    newDriver = Object.assign({}, obj);
+    newDriver[key] = value
+
+    return newDriver;
+};
+    
